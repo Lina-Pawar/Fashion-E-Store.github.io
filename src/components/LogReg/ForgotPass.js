8403d38255ae6cfe59b578071b5bcd9d16dc9910
Amit3108 <61343929+Amit3108@users.noreply.github.com>
2021-08-02 10:55:20 +0530

    commit

--- src/components/LogReg/ForgotPass.js
+++ src/components/LogReg/ForgotPass.js
@@ -1,7 +1,59 @@
 import React, { useState } from "react";
 import "./ForgotPass.css";
-
+import { Link } from "react-router-dom";
 function ForgotPass() {
+  const [user, setUser] = useState({
+    username:""
+});
+let handleChange = (e) => {
+  let name = e.target.name;
+  let value = e.target.value;
+  user[name] = value;
+  setUser(user);
+}
+
+const onSubmit = e => {
+
+ var element =  document.getElementById("succesBTN");
+ var inputData = document.getElementById("inputType");
+  
+  element.style.backgroundColor ="rgba(0,0,0,0.7)";
+  element.innerHTML  = "Checking...";
+  element.style.cursor="not-allowed";
+  inputData.disabled = true;
+  element.disabled = true;
+
+   var myFunctions = function(){
+       if(captcha === user.username)
+       {
+         element.style.background   = "-webkit-gradient(linear,left top, right top,from(#d4af37),color-stop(30%,#f9f095),color-stop(40%,#d4af37),color-stop(80%,#f9f095),color-stop(90%,#d4af37))";
+         element.style.color ="black"
+         element.innerHTML  = "Captcha Verified";
+         element.disabled = true;
+         element.style.cursor = "not-allowed";
+         element.style.fontWeight="bold";
+       }
+       else
+       {
+         element.style.backgroundColor   = "red";
+         element.style.cursor = "not-allowed";
+         element.innerHTML  = "Not Matched";
+         element.disabled = true;
+
+         var myFunction = function(){
+           element.style.backgroundColor   = "black";
+           element.style.cursor = "pointer";
+           element.innerHTML  = "Verify";
+           element.disabled = false;
+           inputData.disabled = false;
+           inputData.value ='';
+         };
+         setTimeout(myFunction,3000);
+       }
+     }   
+     setTimeout(myFunctions,3000); 
+};
+
   const characters = "abc123";
 
   function generateString(length) {
@@ -13,11 +65,9 @@ function ForgotPass() {
     return result;
   }
 
-  const captcha = generateString(6); // Function called here and save in captcha variable
-  // kal karunga
-  const [visible, setVisibility] = useState(false);
-  const Icon = visible ? "fas fa-check" : "fas fa-times";
-  //
+  const captcha = generateString(6); 
+  
+  
   return (
     <div className="forgotpass" id="fp">
       <br /><br />
@@ -62,18 +112,21 @@ function ForgotPass() {
             id="inputType"
             className="fpinput"
             placeholder="Enter Captcha"
-            name="username"
+            name="username"  onChange={handleChange}
             autoComplete="off"
           />
-          <span
-            className={Icon}
-            onClick={() => setVisibility((visibility) => !visibility)}
-          />
+          
+          <Link to="/login">
           <button className="fpbutton" onClick="login()">
+            Back
+          </button>
+          </Link>
+          <button className="fpbutton" id="succesBTN" onClick={onSubmit}>
             Verify
           </button>
-          <button className="fpbutton" onClick="login()">
-            Reset
+          <br />
+          <button className="fpbutton" style={{maxWidth:"440px"}}>
+            Reset Password
           </button>
         </form>
       </div>
