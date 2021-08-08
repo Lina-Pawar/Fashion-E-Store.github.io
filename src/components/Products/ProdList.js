8340b557f391c77d94286cbd681dc67c0f376869
Amit3108 <61343929+Amit3108@users.noreply.github.com>
2021-08-08 20:32:42 +0530

    commit

--- src/components/Products/ProdList.js
+++ src/components/Products/ProdList.js
@@ -1,61 +1,77 @@
+import demo1 from "../../imgs/c1.png";
+import demo2 from "../../imgs/c2.png";
+import demo3 from "../../imgs/c3.png";
+import demo4 from "../../imgs/c4.png";
 export const ProdList = [
     {
-        id:1,
+        id:0,
+        image:demo1,
         name:'Men Shirt',
         price:500
     },
     {
-        id:2,
+        id:1,
+        image:demo2,
         name:'Men T-Shirt',
         price:300
     },
     {
-        id:3,
+        id:2,
+        image:demo3,
         name:'Men Shoe',
         price:200
     },
     {
-        id:4,
+        id:3,
+        image:demo4,
         name:'Women Shoe',
         price:600
     },
     {
-        id:5,
+        id:4,
+        image:demo4,
         name:'Women Top',
         price:500
     },
     {
-        id:6,
+        id:5,
+        image:demo3,
         name:'Kurt',
         price:350
     },
     {
-        id:7,
+        id:6,
+        image:demo2,
         name:'Men Cap',
         price:100
     },
     {
-        id:8,
+        id:7,
+        image:demo1,
         name:'Kids Shoes',
         price:200
     },
     {
-        id:9,
+        id:8,
+        image:demo3,
         name:'Reebok Shoes',
         price:200
     },
     {
-        id:10,
+        id:9,
+        image:demo2,
         name:'Traditional',
         price:750
     },
     {
-        id:11,
+        id:10,
+        image:demo1,
         name:'Boys Pants',
         price:600
     },
     {
-        id:12,
+        id:11,
+        image:demo4,
         name:'Kids Pants',
         price:150
     }
