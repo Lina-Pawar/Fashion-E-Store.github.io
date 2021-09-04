9b6b191960b63ffaa7c6b3f95d866b99d1bb5ba9
Lina-Pawar <lpawar2911@gmail.com>
2021-09-04 21:49:56 +0530

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,28 +27,31 @@ const Navbar = () => {
   const [isVisible1, setVisibility1] = useState(false);
   const [isVisible2, setVisibility2] = useState(false);
   const DropDown1 = isVisible1 ? "showdd" : "hidedd";
-  const arrow1 = isVisible1 ? "fas fa-angle-up" : "fas fa-angle-down" ;
+  const arrow1 = isVisible1 ? "fas fa-angle-up" : "fas fa-angle-down";
   const DropDown2 = isVisible2 ? "showdd" : "hidedd";
-  const arrow2 = isVisible2 ? "fas fa-angle-up" : "fas fa-angle-down" ;
+  const arrow2 = isVisible2 ? "fas fa-angle-up" : "fas fa-angle-down";
+  var items=document.getElementsByClassName("list-item");
+  var names=document.getElementsByClassName("productname");
+  function searchresult(){
+    var search=document.getElementById("search").value;
+    for(var i=0;i<names.length;i++){
+      if(names[i].innerHTML.toLowerCase().match(search.toLowerCase())){
+        items[i].style.display="block";
+      }else{
+        items[i].style.display="none";
+      }
+    }
+  }
   return (
     <nav className="navbar">
-      <Link to= "/home" className="alogo" ><img src={navlogo} alt="Nav Logo" className="logo" /></Link>
-      <input
-        className="search"
-        type="text"
-        id="search"
-        placeholder="Search"
-      />
+      <Link to= "/home" className="alogo"><img src={navlogo} alt="Nav Logo" className="logo"/></Link>
+      <input className="search" type="text" id="search" placeholder="Search" onChange={searchresult}/>
       <span className="fas fa-search"></span>
-      <ul
-        className={isMobile ? "nav-links-mobile" : "nav-links"}
-        onClick={() => setIsMobile(false)}
-      >
+      <ul className={isMobile ? "nav-links-mobile" : "nav-links"} onClick={() => setIsMobile(false)}>
         <li className="profile" onClick={() => setVisibility1((visible) => !visible)}>
           <i className="fas fa-user-circle"></i>
           <span className="moblink">&nbsp;&nbsp;Profile</span>
-          &nbsp;&nbsp;<i className={arrow1} ></i>
-
+          &nbsp;&nbsp;<i className={arrow1}></i>
         </li>
         <div className={DropDown1}>
             <div><Link to="/home" className="notlink1"><p style={{fontSize:"20px",color:"white"}} id="fname"></p></Link></div>
@@ -57,15 +60,14 @@ const Navbar = () => {
         <li className="notify" onClick={() => setVisibility2((visible) => !visible)}>
           <i className="fas fa-bell"></i>
           <span className="moblink">&nbsp;&nbsp;Notification</span>
-          &nbsp;&nbsp;<i className={arrow2} ></i>
-          
+          &nbsp;&nbsp;<i className={arrow2}></i>
         </li>
         <div className={DropDown2}>
             <div><Link to="/women" className="notlink">
               <p style={{color:"white"}}><img src={off} alt="off"/>&nbsp;&nbsp;50% off on women tops today!</p></Link>
             </div>
             <div><Link to="/men" className="notlink">
-              <p style={{color:"white"}}><img src={oneOnone} alt="1on1" />&nbsp;&nbsp;Buy 1 get 1 free on Men t-shirts.</p></Link>
+              <p style={{color:"white"}}><img src={oneOnone} alt="1on1"/>&nbsp;&nbsp;Buy 1 get 1 free on Men t-shirts.</p></Link>
             </div>
             <div><Link to="/kids" className="notlink">
               <p style={{color:"white"}}><img src={free} alt="free" style={{width:90}}/>&nbsp;&nbsp;Free shipping on all kids wear!</p></Link>
@@ -91,10 +93,7 @@ const Navbar = () => {
           </li>
         </button>
       </ul>
-      <button
-        className="mobile-menu-icon"
-        onClick={() => setIsMobile(!isMobile)}
-      >
+      <button className="mobile-menu-icon" onClick={() => setIsMobile(!isMobile)}>
         {isMobile ? (
           <i className="fas fa-times"></i>
         ) : (
